ba0bad70b35406faa1c231c656b4cb0ccc94f475
tristanjgriffin <tgriff34@uncc.edu>
2018-02-06 13:40:59 -0500

    Added Default and Create Contact View

--- app/src/main/java/com/example/tristan/android_projects/MainActivity.java
+++ app/src/main/java/com/example/tristan/android_projects/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.tristan.android_projects;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.newButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, newActivity.class);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/tristan/android_projects/newActivity.java
@@ -0,0 +1,15 @@
+package com.example.tristan.android_projects;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class newActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+
+
+    }
+}
