2132e8c549874cbca48a837aaaba64b41483e1c3
tristanjgriffin <tgriff34@uncc.edu>
2018-02-06 15:44:21 -0500

    Added list view for contacts

--- app/src/main/java/com/example/tristan/android_projects/ContactAdapter.java
+++ app/src/main/java/com/example/tristan/android_projects/ContactAdapter.java
@@ -1,8 +1,46 @@
 package com.example.tristan.android_projects;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
 /**
  * Created by Tristan on 2/6/18.
  */
 
-public class ContactAdapter {
+public class ContactAdapter extends ArrayAdapter<Contact> {
+    public ContactAdapter(Context context, ArrayList<Contact> contacts) {
+        super(context, 0, contacts);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Contact contact = getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_contact, parent, false);
+        }
+
+        TextView textViewFirst = (TextView) convertView.findViewById(R.id.firstListView);
+        TextView textViewLast = (TextView) convertView.findViewById(R.id.lastListView);
+        TextView textViewPhone = (TextView) convertView.findViewById(R.id.phoneListView);
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.contactAvatar);
+
+        textViewFirst.setText(contact.user_firstName);
+        textViewLast.setText(contact.user_lastName);
+        textViewPhone.setText(contact.user_phone);
+
+        return convertView;
+    }
 }
