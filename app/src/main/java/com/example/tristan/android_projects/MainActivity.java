68325ad27637be092932e7384d65da907e5d1b88
tristanjgriffin <tgriff34@uncc.edu>
2018-02-12 13:08:40 -0500

    Mod5Prac

--- app/src/main/java/com/example/tristan/android_projects/MainActivity.java
+++ app/src/main/java/com/example/tristan/android_projects/MainActivity.java
@@ -1,7 +1,22 @@
 package com.example.tristan.android_projects;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +24,92 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new GetDataAsync().execute("http://api.theappsdr.com/simple.php");
+        new GetImageAsync((ImageView) findViewById(R.id.imageView2)).execute("https://upload.wikimedia.org/wikipedia/commons/6/66/Android_robot.png");
+    }
+
+    private boolean isConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+
+        if (networkInfo.isConnected() && networkInfo != null) {
+            return true;
+        }
+        return false;
+    }
+
+    private class GetImageAsync extends AsyncTask<String, Void, Bitmap> {
+
+        ImageView imageView;
+
+        public GetImageAsync(ImageView iv) {
+            this.imageView = iv;
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            try {
+                URL url = new URL(strings[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                Bitmap image;
+
+                image = BitmapFactory.decodeStream(connection.getInputStream());
+
+                return image;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+
+    private class GetDataAsync extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                URL url = new URL(strings[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line = "";
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line + "\n");
+                }
+
+                bufferedReader.close();
+                connection.disconnect();
+
+                return stringBuilder.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+
+            if (s != null) {
+                Log.d("demo", s);
+            } else {
+                Log.d("demo", "No result");
+            }
+
+        }
     }
 }
